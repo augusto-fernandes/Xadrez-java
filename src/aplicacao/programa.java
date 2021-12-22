ab645ee1410e38344213473da20bd0170d651d76
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2021-12-22 14:05:20 -0300

    primeira classe: posicao

--- src/aplicacao/programa.java
+++ src/aplicacao/programa.java
@@ -1,9 +1,12 @@
 package aplicacao;
 
+import tabuleiro.posicao;
+
 public class programa {
 
 	public static void main(String[] args) {
-		System.out.println("bundinha");
+		posicao pos = new posicao(3, 5);
+		System.out.println(pos);
 	}
 
 }
